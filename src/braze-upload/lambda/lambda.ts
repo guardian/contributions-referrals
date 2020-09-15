c8235b97a7acfa1084b69930f0e08dc124956f64
Tom Forbes <tom.forbes@theguardian.com>
2020-09-15 11:53:53 +0100

    try old thrift

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 // TODO - resolve thrift -> thrift/src/thrift
-const Thrift = require('thrift/src/thrift');
+// const Thrift = require('thrift/src/thrift');
+const Thrift = require('thrift');
 const acquisition_types = require('../gen-nodejs/acquisition_types');
 
 export async function handler(event: any, context: any): Promise<null> {
