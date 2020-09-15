334186b9a31c5cc72fbfa06ffa31ee04ba84780e
Tom Forbes <tom.forbes@theguardian.com>
2020-09-15 13:02:06 +0100

    debug

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -1,20 +1,18 @@
 const AWS = require('aws-sdk');
-// TODO - resolve thrift -> thrift/src/thrift
-// const Thrift = require('thrift/src/thrift');
-const Thrift = require('thrift');
-const acquisition_types = require('../gen-nodejs/acquisition_types');
+// const Thrift = require('thrift');
+// const acquisition_types = require('../gen-nodejs/acquisition_types');
 
 export async function handler(event: any, context: any): Promise<null> {
     console.log("events:", event.Records.length);
     event.Records.map(record => {
         const payload = new Buffer(record.kinesis.data, 'base64');
         console.log("payload", payload);
-        const transport = new Thrift.TFramedTransport(payload);
-        const protocol = new Thrift.TCompactProtocol(transport);
-        const acquisition = new acquisition_types.Event();
-
-        const result = acquisition.read(protocol);
-        console.log("result",result)
+        // const transport = new Thrift.TFramedTransport(payload);
+        // const protocol = new Thrift.TCompactProtocol(transport);
+        // const acquisition = new acquisition_types.Event();
+        //
+        // const result = acquisition.read(protocol);
+        // console.log("result",result)
     });
     return Promise.resolve(null);
 }
