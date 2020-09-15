285091747977321488c7e11665763085bd1402d8
Tom Forbes <tom.forbes@theguardian.com>
2020-09-15 13:15:09 +0100

    fix lambda path

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -1,18 +1,18 @@
 const AWS = require('aws-sdk');
-// const Thrift = require('thrift');
-// const acquisition_types = require('../gen-nodejs/acquisition_types');
+const Thrift = require('thrift');
+const acquisition_types = require('../gen-nodejs/acquisition_types');
 
 export async function handler(event: any, context: any): Promise<null> {
     console.log("events:", event.Records.length);
     event.Records.map(record => {
         const payload = new Buffer(record.kinesis.data, 'base64');
         console.log("payload", payload);
-        // const transport = new Thrift.TFramedTransport(payload);
-        // const protocol = new Thrift.TCompactProtocol(transport);
-        // const acquisition = new acquisition_types.Event();
-        //
-        // const result = acquisition.read(protocol);
-        // console.log("result",result)
+        const transport = new Thrift.TFramedTransport(payload);
+        const protocol = new Thrift.TCompactProtocol(transport);
+        const acquisition = new acquisition_types.Event();
+
+        const result = acquisition.read(protocol);
+        console.log("result",result)
     });
     return Promise.resolve(null);
 }
