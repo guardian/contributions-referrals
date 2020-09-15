b16499c6608b7780d680b18d5442597b04ba09e0
Tom Forbes <tom.forbes@theguardian.com>
2020-09-15 10:25:02 +0100

    payload

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -1,5 +1,9 @@
 
 export async function handler(event: any, context: any): Promise<null> {
-    console.log("event", event);
+    console.log("events:", event.Records.length);
+    event.Records.map(record => {
+        const payload = new Buffer(record.kinesis.data, 'base64');
+        console.log("payload", payload);
+    });
     return Promise.resolve(null);
 }
