1b9979a9b41d6beb1a944ee5832890002f48d8e7
Tom Forbes <tom.forbes@theguardian.com>
2020-09-16 09:25:50 +0100

    use thrift-serializer

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -1,18 +1,16 @@
 const AWS = require('aws-sdk');
-const Thrift = require('thrift');
 const acquisition_types = require('../gen-nodejs/acquisition_types');
+const serializer = require('thrift-serializer');
 
 export async function handler(event: any, context: any): Promise<null> {
     console.log("events:", JSON.stringify(event));
     event.Records.map(record => {
-        const payload = new Buffer(record.kinesis.data, 'base64');
-        console.log("payload", payload);
-        const transport = new Thrift.TFramedTransport(payload);
-        const protocol = new Thrift.TCompactProtocol(transport);
-        const acquisition = new acquisition_types.Acquisition();
-
-        const result = acquisition.read(protocol);
-        console.log("result",result)
+        serializer.read(acquisition_types.Acquisition, record.kinesis.data, function (err, msg) {
+            if (err) {
+                console.log("error", err)
+            }
+            console.log("result", JSON.stringify(msg));
+        });
     });
     return Promise.resolve(null);
 }
