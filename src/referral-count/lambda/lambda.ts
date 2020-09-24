63cae8cdf0183ba578d64b32a10acf6c0b7cf35c
Tom Forbes <tom.forbes@theguardian.com>
2020-09-24 10:42:24 +0100

    querystring

--- src/referral-count/lambda/lambda.ts
+++ src/referral-count/lambda/lambda.ts
@@ -2,14 +2,14 @@ import {logInfo} from "../../lib/log";
 import {isRunningLocally} from "../../lib/stage";
 
 interface ReferralCountRequest {
-    braze_uuid: string,
-    campaign_id: string,
-    from_date: string  //TODO - or a number?
+    brazeUuid: string,
+    campaignId: string,
+    fromDate: string  //TODO - or a number?
 }
 
 export async function handler(event: any, context: any): Promise<object> {
     logInfo('event: ', event);
-    const parsedEvent = isRunningLocally() ? event : JSON.parse(event.body);
-    logInfo('parsed: ', parsedEvent);
-    return parsedEvent;
+    const parsedQueryString = isRunningLocally() ? event : JSON.parse(event.queryStringParameters);
+    logInfo('parsed: ', parsedQueryString);
+    return parsedQueryString;
 }
