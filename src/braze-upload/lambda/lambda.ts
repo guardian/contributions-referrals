b0c3e183e1fd8dbc8819b4975329976a832593ba
Tom Forbes <tom.forbes@theguardian.com>
2020-10-16 13:25:11 +0100

    script for manually sending a referralCode

--- src/braze-upload/lambda/lambda.ts
+++ src/braze-upload/lambda/lambda.ts
@@ -63,9 +63,45 @@ const getReferralCodeFromThriftBytes = (rawThriftData: any): Promise<string | nu
         });
     });
 
-export async function handler(event: Event, context: any): Promise<any> {
-    const config = await lambdaConfigPromise;
+export const processReferralCode = async (referralCode: string): Promise<void> => {
+    logInfo(`Processing referralCode ${referralCode}`);
+
     const pool = await dbConnectionPool;
+    const config = await lambdaConfigPromise;
+
+    // Fetch the braze uuid
+    const referralDataLookupResult: QueryResult = await fetchReferralData(referralCode, pool);
+
+    const referralData = referralDataLookupResult.rows[0];
+    if (!referralData) {
+        return Promise.reject(`No brazeUuid found for referralCode ${referralCode}`);
+    }
+
+    // Write the successful referral
+    const writeResult: QueryResult = await writeSuccessfulReferral(
+        {
+            brazeUuid: referralData.braze_uuid,
+            referralCode: referralCode,
+            campaignId: referralData.campaign_id,
+        },
+        pool
+    );
+    if (writeResult.rows.length <= 0) {
+        return Promise.reject(`Failed to write successful referral for code ${referralCode}`);
+    }
+
+    // Fetch the distinct set of campaignIds for this braze user
+    const campaignIdsResult: QueryResult = await fetchCampaignIds(referralData.braze_uuid, pool);
+    if (campaignIdsResult.rows.length <= 0) {
+        return Promise.reject(`No campaignIds found for brazeUuid ${referralData.braze_uuid}`);
+    }
+
+    const campaignIds = campaignIdsResult.rows.map(row => row.campaign_id);
+
+    return sendCampaignIdsToBraze(campaignIds, referralData.braze_uuid, config.brazeKey);
+};
+
+export async function handler(event: Event, context: any): Promise<any> {
 
     const maybeReferralCodes: (string | null)[] = await Promise.all(
         event.Records.map(record => getReferralCodeFromThriftBytes(record.kinesis.data))
@@ -74,40 +110,7 @@ export async function handler(event: Event, context: any): Promise<any> {
     const resultPromises = maybeReferralCodes
         .filter(maybeReferralCode => !!maybeReferralCode)
         .map(c => c as string)  // typescript doesn't know that the above line filters to strings only
-        .map(async (referralCode: string) => {
-            logInfo(`Processing referralCode ${referralCode}`);
-
-            // Fetch the braze uuid
-            const referralDataLookupResult: QueryResult = await fetchReferralData(referralCode, pool);
-
-            const referralData = referralDataLookupResult.rows[0];
-            if (!referralData) {
-                return Promise.reject(`No brazeUuid found for referralCode ${referralCode}`);
-            }
-
-            // Write the successful referral
-            const writeResult: QueryResult = await writeSuccessfulReferral(
-                {
-                    brazeUuid: referralData.braze_uuid,
-                    referralCode: referralCode,
-                    campaignId: referralData.campaign_id,
-                },
-                pool
-            );
-            if (writeResult.rows.length <= 0) {
-                return Promise.reject(`Failed to write successful referral for code ${referralCode}`);
-            }
-
-            // Fetch the distinct set of campaignIds for this braze user
-            const campaignIdsResult: QueryResult = await fetchCampaignIds(referralData.braze_uuid, pool);
-            if (campaignIdsResult.rows.length <= 0) {
-                return Promise.reject(`No campaignIds found for brazeUuid ${referralData.braze_uuid}`);
-            }
-
-            const campaignIds = campaignIdsResult.rows.map(row => row.campaign_id);
-
-            return sendCampaignIdsToBraze(campaignIds, referralData.braze_uuid, config.brazeKey);
-        });
+        .map(processReferralCode);
 
     return Promise.all(resultPromises);
 }
